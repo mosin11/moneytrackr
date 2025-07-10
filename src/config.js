189eea3f6e19bfdb9538ac201cbde6d81c4c5cf8
rakishop <rakishop133@gmail.com>
2025-07-10 23:19:03 +0530

    user based auth added

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // src/config.js
 
-const API_BASE_URL = 'http://localhost:5000'; 
- //const API_BASE_URL = 'https://cashbook-backup-server.onrender.com'; // Use this in production 
+//const API_BASE_URL = 'http://localhost:5000'; 
+ const API_BASE_URL = 'https://cashbook-backup-server.onrender.com'; // Use this in production 
 
 const API_ENDPOINTS = {
   // 🔐 Auth APIs
